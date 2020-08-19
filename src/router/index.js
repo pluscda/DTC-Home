7edef673bf1999f2cbf1a658fde8fd1a17b5103e
alex <pluscda@gmail.com>
2020-08-19 15:41:46 +0800

    g

--- src/router/index.js
+++ src/router/index.js
@@ -23,6 +23,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ "../views/AboutXl.vue"),
   },
+  {
+    path: "/news",
+    name: "news",
+    component: () => import(/* webpackChunkName: "about" */ "../views/NewsXl.vue"),
+  },
 ];
 
 const router = new VueRouter({

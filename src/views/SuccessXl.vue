3d175907260fe30feaed15228a29bb85c4a2d30f
alex <pluscda@gmail.com>
2020-08-20 16:43:24 +0800

    g

--- src/views/SuccessXl.vue
+++ src/views/SuccessXl.vue
@@ -2,53 +2,22 @@
   <section class="about-us">
     <DtcXlNavWhiteBar></DtcXlNavWhiteBar>
     <div class="banner">
-      <img src="news_title.svg" width="140" />
+      <img src="client_title.svg" width="140" />
     </div>
-
-    <img src="2019_news.jpg" class="dtc-body mt-4" v-if="currentPage == 1" />
-    <img src="2017_news.jpg" class="dtc-body mt-4" v-if="currentPage == 2" />
-    <img src="2015_news.jpg" class="dtc-body mt-4" v-if="currentPage == 3" />
-    <img src="2012_news.jpg" class="dtc-body mt-4" v-if="currentPage == 4" />
-
-    <div class="pages-dtc mt-4 mb-4">
-      <b-pagination-nav
-        :link-gen="linkGen"
-        use-router
-        v-model="currentPage"
-        :number-of-pages="4"
-        prev-text="上一頁"
-        next-text="下一頁"
-      ></b-pagination-nav>
-    </div>
+    <img src="client_content.svg" class="dtc-body" />
   </section>
 </template>
 
 <script>
-import DtcXlNavWhiteBar from '@/components/DtcXlNavWhiteBar.vue';
+import DtcXlNavWhiteBar from "@/components/DtcXlNavWhiteBar.vue";
 
 export default {
   data() {
-    return {
-      currentPage: 1,
-    };
+    return {};
   },
-  components:{
-    DtcXlNavWhiteBar
+  components: {
+    DtcXlNavWhiteBar,
   },
-  methods:{
-    linkGen(){
-      return "news";
-    }
-  },
-  watch:{
-    currentPage(){
-     window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: 'smooth'
-    });
-    }
-  }
 };
 </script>
 
@@ -59,31 +28,21 @@ export default {
   color: black;
   display: flex;
   flex-direction: column;
-  text-align: center;
 }
 .dtc-body {
   display: block;
   width: 1200px;
-  margin: 0 auto;
+  margin: 4rem auto;
 }
 .banner {
   width: 100vw;
   height: 380px;
-  background: url("/banner.jpg") no-repeat center center;
+  background: url("/client_banner.svg") no-repeat center center;
   background-size: cover;
   display: grid;
   place-items: center;
   > img {
     transform: translateY(40px);
-  }
-}
-
-.pages-dtc {
-  display: grid;
-  min-width: 1200px;
-  place-items: center;
-  > div {
-    transform: translateX(200px);
   }
 }
 

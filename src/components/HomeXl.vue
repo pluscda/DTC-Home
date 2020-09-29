2541dfda9aea841bf94b6058b101a4d39215a29c
alex <pluscda@gmail.com>
2020-09-29 14:12:39 +0800

    g

--- src/components/HomeXl.vue
+++ src/components/HomeXl.vue
@@ -1,117 +1,124 @@
 <template>
-  <div class="dashboard6" id="list">
-    <div id="div1">1</div>
-    <div id="div2">2</div>
-    <div id="div3">3</div>
-    <div id="div4">4</div>
-    <div id="div5">5</div>
-    <div id="div6">6</div>
-  </div>
+  <section>
+    <div class="dashboard6" id="list" v-if="type == 6">
+      <div id="div1">1</div>
+      <div id="div2">2</div>
+      <div id="div3">3</div>
+      <div id="div4">4</div>
+      <div id="div5">5</div>
+      <div id="div6">6</div>
+    </div>
+    <div class="dashboard4" id="list" v-if="type == 4">
+      <div id="div1">1</div>
+      <div id="div2">2</div>
+      <div id="div3">3</div>
+      <div id="div4">4</div>
+    </div>
+    <div class="dashboard" id="list" v-if="type == 1">
+      <div id="div1">1</div>
+    </div>
+  </section>
 </template>
 
 <script>
+import mousetrap from "mousetrap";
 export default {
   name: "home",
   components: {},
   data() {
     return {
       items: ["a", "b", "c", "d"],
+      type: 6,
     };
   },
   computed: {
     isLogin() {},
   },
-  methods: {},
+  methods: {
+    viewFullScreen() {
+      var el = document.getElementById("list");
+      el.requestFullscreen();
+    },
+    enableDnD() {
+      function sortable(section, onUpdate) {
+        var dragEl, nextEl, newPos, dragGhost;
 
-  async mounted() {
-    function sortable(section, onUpdate) {
-      var dragEl, nextEl, newPos, dragGhost;
-
-      let oldPos = [...section.children].map((item) => {
-        item.draggable = true;
-        let pos = document.getElementById(item.id).getBoundingClientRect();
-        return pos;
-      });
-
-      function _onDragOver(e) {
-        e.preventDefault();
-        e.dataTransfer.dropEffect = "move";
-
-        var target = e.target;
-        if (target && target !== dragEl && target.nodeName == "DIV") {
-          if (target.classList.contains("inside")) {
-            e.stopPropagation();
-          } else {
-            //getBoundinClientRect contains location-info about the element (relative to the viewport)
-            var targetPos = target.getBoundingClientRect();
-            //checking that dragEl is dragged over half the target y-axis or x-axis. (therefor the .5)
-            var next = (e.clientY - targetPos.top) / (targetPos.bottom - targetPos.top) > 0.5 || (e.clientX - targetPos.left) / (targetPos.right - targetPos.left) > 0.5;
-
-            section.insertBefore(dragEl, (next && target.nextSibling) || target);
-
-            /*  console.log("oldPos:" + JSON.stringify(oldPos));
-             console.log("newPos:" + JSON.stringify(newPos)); */
-            /* console.log(newPos.top === oldPos.top ? 'They are the same' : 'Not the same'); */
-            console.log(oldPos);
-          }
-        }
-      }
-
-      function _onDragEnd(evt) {
-        evt.preventDefault();
-        newPos = [...section.children].map((child) => {
-          let pos = document.getElementById(child.id).getBoundingClientRect();
+        let oldPos = [...section.children].map((item) => {
+          item.draggable = true;
+          let pos = document.getElementById(item.id).getBoundingClientRect();
           return pos;
         });
-        console.log(newPos);
-        dragEl.classList.remove("ghost");
-        section.removeEventListener("dragover", _onDragOver, false);
-        section.removeEventListener("dragend", _onDragEnd, false);
 
-        nextEl !== dragEl.nextSibling ? onUpdate(dragEl) : false;
+        function _onDragOver(e) {
+          e.preventDefault();
+          e.dataTransfer.dropEffect = "move";
+
+          var target = e.target;
+          if (target && target !== dragEl && target.nodeName == "DIV") {
+            if (target.classList.contains("inside")) {
+              e.stopPropagation();
+            } else {
+              //getBoundinClientRect contains location-info about the element (relative to the viewport)
+              var targetPos = target.getBoundingClientRect();
+              //checking that dragEl is dragged over half the target y-axis or x-axis. (therefor the .5)
+              var next =
+                (e.clientY - targetPos.top) / (targetPos.bottom - targetPos.top) > 0.5 || (e.clientX - targetPos.left) / (targetPos.right - targetPos.left) > 0.5;
+
+              section.insertBefore(dragEl, (next && target.nextSibling) || target);
+            }
+          }
+        }
+
+        function _onDragEnd(evt) {
+          evt.preventDefault();
+          newPos = [...section.children].map((child) => {
+            let pos = document.getElementById(child.id).getBoundingClientRect();
+            return pos;
+          });
+          dragEl.classList.remove("ghost");
+          section.removeEventListener("dragover", _onDragOver, false);
+          section.removeEventListener("dragend", _onDragEnd, false);
+
+          nextEl !== dragEl.nextSibling ? onUpdate(dragEl) : false;
+        }
+
+        section.addEventListener("dragstart", function(e) {
+          dragEl = e.target;
+          nextEl = dragEl.nextSibling;
+          e.dataTransfer.effectAllowed = "move";
+          e.dataTransfer.setData("Text", dragEl.textContent);
+          section.addEventListener("dragover", _onDragOver, false);
+          section.addEventListener("dragend", _onDragEnd, false);
+
+          setTimeout(function() {
+            dragEl.classList.add("ghost");
+          }, 0);
+        });
       }
 
-      section.addEventListener("dragstart", function(e) {
-        dragEl = e.target;
-        nextEl = dragEl.nextSibling;
-        /* dragGhost = dragEl.cloneNode(true);
-        dragGhost.classList.add('hidden-drag-ghost'); */
-
-        /*  document.body.appendChild(dragGhost);
-        e.dataTransfer.setDragImage(dragGhost, 0, 0); */
-
-        e.dataTransfer.effectAllowed = "move";
-        e.dataTransfer.setData("Text", dragEl.textContent);
-
-        section.addEventListener("dragover", _onDragOver, false);
-        section.addEventListener("dragend", _onDragEnd, false);
-
-        setTimeout(function() {
-          dragEl.classList.add("ghost");
-        }, 0);
+      sortable(document.getElementById("list"), function(item) {
+        /* console.log(item); */
       });
-    }
-
-    sortable(document.getElementById("list"), function(item) {
-      /* console.log(item); */
-    });
+    },
   },
-  async beforeCreate() {},
+
+  async mounted() {
+    this.enableDnD();
+    mousetrap.bind("f4", () => {
+      this.type = 4;
+      requestAnimationFrame(() => this.enableDnD());
+    });
+    mousetrap.bind("f6", () => {
+      this.type = 6;
+      requestAnimationFrame(() => this.enableDnD());
+    });
+    mousetrap.bind("f1", () => {
+      this.type = 1;
+    });
+    mousetrap.bind("f8", () => this.viewFullScreen());
+  },
+  async created() {},
 };
-
-/* still in progress */
-
-/* The setData() method is used to add an item to the drag data, as shown in the following example.
-
-function dragstart_handler(ev) {
-  // Add the drag data
-  ev.dataTransfer.setData("text/plain", ev.target.id);
-  ev.dataTransfer.setData("text/html", "<p>Example paragraph</p>");
-  ev.dataTransfer.setData("text/uri-list", "http://developer.mozilla.org"); */
-
-/* you may succeed this a hacky solution. The native draggability doesn't allow CSS styles like: opacity:0;, visibility:hidden or display:none.
-But you can do it using: transform:translateX(-9999px).
-I've updated your JSFiddle with the solution. */
 </script>
 
 <style lang="scss">
@@ -120,7 +127,7 @@ I've updated your JSFiddle with the solution. */
 }
 .dashboard6,
 .dashboard4,
-.dashboard1 {
+.dashboard {
   margin: 0px;
   padding: 10px;
   list-style: none;
@@ -128,7 +135,7 @@ I've updated your JSFiddle with the solution. */
   -moz-user-select: none;
   user-select: none;
   display: grid;
-  grid-gap: 15px;
+  grid-gap: 10px;
   grid-template-rows: repeat(2, 1fr);
   grid-template-columns: repeat(3, 1fr);
   grid-auto-flow: row dense;
@@ -141,7 +148,23 @@ I've updated your JSFiddle with the solution. */
     border: 1px solid black;
     font-size: 24px;
     color: black;
-    background: #bababa;
+    background: white;
+    border-radius: 10px;
+  }
+}
+
+.dashboard4 {
+  grid-template-columns: repeat(2, 1fr);
+}
+
+.dashboard {
+  grid-template-columns: repeat(1, 1fr);
+  padding: 0;
+  margin: 0;
+  grid-gap: 0px;
+  > div {
+    height: 100vh;
+    border: none;
   }
 }
 .ghost {

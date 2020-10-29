b9d14e7722c6396f088c173fdb71c5d6e5912ee5
alex <pluscda@gmail.com>
2020-10-29 14:40:50 +0800

    g

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,31 @@
 <template>
-  <div id="app">
+  <div id="app" :style="showPrivacy || showTerm ? 'overflow:hidden !important;' : ''">
     <router-view />
     <footer>
       <main>
-        <h5 style="transform:translateY(10px)">DATACOM TECH.</h5>
+        <h5 style="transform:translate(-3px, 10px) !important;">DATACOM TECH.</h5>
         <nav>
-          <h6>隱私權政策</h6>
-          <div></div>
-          <h6>網站安全政策</h6>
-          <div></div>
-          <h6>使用者條款</h6>
+          <h6 @click="showPrivacy = true" style="transform:translateX(15px)">隱私權政策</h6>
+          <div style="transform:translateX(15px)"></div>
+          <h6 @click="showTerm = true" style="transform:translateX(-15px)">使用者條款</h6>
         </nav>
-        <div
-          style="color:white;transform:translate(20px, -7px);font-size:12px;"
-        >Copyright©2020 DATACOM. All rights reserved.</div>
+        <div style="color:white;transform:translate(20px, -7px);font-size:12px;">Copyright©2020 DATACOM. All rights reserved.</div>
       </main>
     </footer>
+    <aside v-if="showPrivacy" class="my-term">
+      <div>
+        <span @click="showPrivacy = false" class="my-close"><i class="fas fa-times-circle"></i></span>
+        <img src="privacy.png" />
+      </div>
+    </aside>
+    <aside v-if="showTerm" class="my-term">
+      <div>
+        <span @click="showTerm = false" class="my-close"><i class="fas fa-times-circle"></i></span>
+        <img src="term.png" />
+      </div>
+    </aside>
   </div>
 </template>
-
 
 <script>
 // @ is an alias to /src
@@ -27,20 +34,25 @@ import DtcXlNavBar from "@/components/DtcXlNavBar.vue";
 export default {
   name: "Home",
   components: {
-    DtcXlNavBar
+    DtcXlNavBar,
   },
-   watch: {
-    '$route' (to, from) {
+  data() {
+    return {
+      showPrivacy: false,
+      showTerm: false,
+    };
+  },
+  watch: {
+    $route(to, from) {
       window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: 'smooth'
-    });
-    }
-  }
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    },
+  },
 };
 </script>
-
 
 <style lang="scss">
 @import "node_modules/bootstrap/scss/bootstrap";
@@ -81,7 +93,7 @@ footer {
     }
     nav {
       display: grid;
-      grid-template-columns: max-content 1px max-content 1px max-content;
+      grid-template-columns: max-content 1px max-content;
       gap: 1rem;
       > div {
         background: white;
@@ -99,6 +111,39 @@ footer {
     color: #f2f2f2;
     cursor: pointer;
     font-size: 14px;
+    margin-left: 2rem;
+  }
+}
+.my-term {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: rgba(#fff, 0.95);
+  display: grid;
+  place-items: center;
+  z-index: 9999999999;
+  > div {
+    width: 100vw;
+    height: 100vh;
+    display: grid;
+    place-items: center;
+    overflow-y: auto;
+    position: relative;
+    img {
+      display: block;
+      z-index: 99999999999;
+      height: 1806px;
+    }
+    .my-close {
+      cursor: pointer;
+      position: sticky;
+      top: 1rem;
+      transform: translateX(500px);
+      color: black;
+      font-size: 24px;
+    }
   }
 }
 </style>

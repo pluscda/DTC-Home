ef311a46f817960773c1ef74047ea2997c537b1f
alex <pluscda@gmail.com>
2020-10-29 14:53:12 +0800

    g

--- src/App.vue
+++ src/App.vue
@@ -12,13 +12,13 @@
         <div style="color:white;transform:translate(20px, -7px);font-size:12px;">Copyright©2020 DATACOM. All rights reserved.</div>
       </main>
     </footer>
-    <aside v-if="showPrivacy" class="my-term">
+    <aside v-show="showPrivacy" class="my-term">
       <div @click="showPrivacy = false">
         <span class="my-close"><i class="fas fa-times-circle"></i></span>
         <img src="privacy.png" />
       </div>
     </aside>
-    <aside v-if="showTerm" class="my-term" @click="showTerm = false">
+    <aside v-show="showTerm" class="my-term" @click="showTerm = false">
       <div @click="showTerm = false">
         <span class="my-close"><i class="fas fa-times-circle"></i></span>
         <img src="term.png" />
